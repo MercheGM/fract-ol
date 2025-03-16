f546435cc1896aaf997f107ce63612f2f65d0a11
Mercedes Garcia Morales <mergarci@c2r11s6.42madrid.com>
2025-03-16 21:26:27 +0100

    20250316: added color_utils and zoom files

---
+++ src/color_utils.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mergarci <mergarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 20:02:34 by mergarci          #+#    #+#             */
+/*   Updated: 2025/03/16 20:04:52 by mergarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	get_rgba(int r, int g, int b, int a)
+{
+	return (r << 24 | g << 16 | b << 8 | a);
+}
+
+// Get the red channel.
+int	get_r(int rgba)
+{
+	return ((rgba >> 24) & 0xFF);
+}
+
+// Get the green channel.
+int	get_g(int rgba)
+{
+	return ((rgba >> 16) & 0xFF);
+}
+
+// Get the blue channel.
+int	get_b(int rgba)
+{
+	return ((rgba >> 8) & 0xFF);
+}
+
+// Get the alpha channel.
+int	get_a(int rgba)
+{
+	return (rgba & 0xFF);
+}
---
+++ src/zoom.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zoom.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mergarci <mergarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 18:59:13 by mergarci          #+#    #+#             */
+/*   Updated: 2025/03/16 19:50:50 by mergarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	zoom_in(t_data *data, double zoom_factor, int x, int y)
+{
+	double	pointer_x;
+	double	pointer_y;
+
+	pointer_x = 0;
+	pointer_y = 0;
+	if ((x != 0) && (y != 0))
+	{
+		pointer_x = data->x_min + (data->x_max - data->x_min) * \
+					((double)x / (WIN_WIDTH - 1));
+		pointer_y = data->y_min + (data->y_max - data->y_min) * \
+					((double)y / (WIN_HEIGHT - 1));
+	}
+	data->x_min = pointer_x + zoom_factor * (data->x_min - pointer_x);
+	data->x_max = pointer_x + zoom_factor * (data->x_max - pointer_x);
+	data->y_min = pointer_y + zoom_factor * (data->y_min - pointer_y);
+	data->y_max = pointer_y + zoom_factor * (data->y_max - pointer_y);
+	data->max_iter += 5;
+}
+
+void	zoom_out(t_data *data, double zoom_factor, int x, int y)
+{
+	double	pointer_x;
+	double	pointer_y;
+
+	pointer_x = 0;
+	pointer_y = 0;
+	if ((x != 0) && (y != 0))
+	{
+		pointer_x = data->x_min + (data->x_max - data->x_min) * \
+					((double)x / (WIN_WIDTH - 1));
+		pointer_y = data->y_max - (data->y_max - data->y_min) * \
+					((double)y / (WIN_HEIGHT - 1));
+	}
+	data->x_min = pointer_x + (data->x_min - pointer_x) / zoom_factor;
+	data->x_max = pointer_x + (data->x_max - pointer_x) / zoom_factor;
+	data->y_min = pointer_y + (data->y_min - pointer_y) / zoom_factor;
+	data->y_max = pointer_y + (data->y_max - pointer_y) / zoom_factor;
+	data->max_iter -= 5;
+}
+
+void	scroll_zoom(double xdelta, double ydelta, void *param)
+{
+	t_data	*data;
+	int		x;
+	int		y;
+
+	data = (t_data *)param;
+	mlx_get_mouse_pos(data->mlx, &x, &y);
+	check_mouse_pos(&x, &y);
+	if (ydelta > 0)
+		zoom_in(data, data->zoom_factor, x, y);
+	else if (ydelta < 0)
+		zoom_out(data, data->zoom_factor, x, y);
+    print_fractal(data);
+}
