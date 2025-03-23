9b545c0b65ccd9fb99974f429f02110a9c689beb
Mercedes Garcia Morales <mergarci@c1r12s6.42madrid.com>
2025-03-23 18:34:19 +0100

    20250323: fixed norminette issue =(

--- src/inputs.c
+++ src/inputs.c
@@ -6,7 +6,7 @@
 /*   By: mergarci <mergarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 19:34:16 by mergarci          #+#    #+#             */
-/*   Updated: 2025/03/23 17:01:13 by mergarci         ###   ########.fr       */
+/*   Updated: 2025/03/23 18:32:58 by mergarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ bool	check_c(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if ((str[i] == '-') && i == 0) 
+		if ((str[i] == '-') && i == 0)
 			i++;
 		else if ((ft_isdigit(str[i])) || (str[i] == '.'))
 			i++;
